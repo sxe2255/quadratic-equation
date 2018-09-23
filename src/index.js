6f381b741611b121a74020dc2f1422cb7336ed20
sxe2255 <sxe2255@gmail.com>
2018-09-23 20:24:27 +0300

    add result 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var d;
+  var array = [];
+  var result = [];
+  var arr = equation.split(' ');
+  
+  for (i = 0; i<=arr.length; i++){
+    if (arr[i] === "-" || arr[i] === "+" ){
+    arr[i] = arr[i] + arr[i+1];	
+    arr[i+1] = "";	
+  }
+  }
+  for (i = 0; i<=arr.length; i++){
+  array[i] = parseInt(arr[i], 10);
+  }
+  for (i=0; i<=array.length; i++){
+  if (isNaN(array[i])) {
+    array.splice(i,i+1);
+  }}
+  
+  d = array[1]*array[1] - 4*array[0]*array[2];
+  result[0] = ((-array[1]) + Math.sqrt(d)) / (2 * array[0]);
+  result[1] = ((-array[1]) - Math.sqrt(d)) / (2 * array[0]);
+  result[0] = Math.round(result[0]);
+  result[1] = Math.round(result[1]);
+  result.sort(function(a, b){return a-b;})
+  return result;
+
+
 }
